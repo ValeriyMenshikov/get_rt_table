f6d0f2e30c94c819e2acfecab156163ec902f070
Valery Menshikov <48724158+ValeriyMenshikov@users.noreply.github.com>
2020-02-12 17:09:12 +0600

    Вынес запросы отдельно

--- sql.py
+++ sql.py
@@ -1,84 +1,21 @@
-import cx_Oracle
-import pandas as pd
-import logging
-
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-
-
-logging.disable(logging.INFO)
-
-class Parameter:
-    def __init__(self, table_name, id_realization_1, sql_query, db, different=False, id_realization_2='0'):
-        self.table_name = table_name
-        self.id_realization_1 = id_realization_1
-        self.id_realization_2 = id_realization_2
-        self.sql_query = sql_query
-        self.db = db
-        self.different = different
-
-
-def get_data_from_table(Parameter):
-    logging.debug(f'Устанавливаем соединение c {Parameter.db} установлено')
-    connection = cx_Oracle.connect(Parameter.db)
-    logging.debug(f'Соединение c {Parameter.db} установлено')
-
-    if Parameter.different:
-        logging.debug('Формируем запрос для выгрузки реализации')
-        sql = Parameter.sql_query + Parameter.id_realization_1
-    else:
-        logging.debug('Формируем запрос для выгрузки результата сравнения реализации')
-        sql = Parameter.sql_query + Parameter.id_realization_1 + '\nminus\n' + Parameter.sql_query + Parameter.id_realization_2
-
-    logging.debug(sql)
-
-    with connection.cursor() as cur:
-        logging.debug('Открываем курсор')
-        cur.execute(sql)
-        # Получаем именования колонок
-        columns = [desc[0] for desc in cur.description]
-
-        # Устанавливаем количество строк для fetch
-        numRows = 10000
-
-        # Счетчик выгруженных строк
-        sum_rows = 0
-
-        while True:
-            rows = cur.fetchmany(numRows)
-            logging.debug('Запрос отправлен формируем датафрейм')
-
-            df = pd.DataFrame(list(rows), columns=columns)
-            # Сначала записываем в файл с заголовком
-            if sum_rows == 0:
-                df.to_csv(f'{Parameter.table_name}.csv', mode='a', header=True, index=False)
-            # Потом дописываем в файл без заголовка
-            else:
-                df.to_csv(f'{Parameter.table_name}.csv', mode='a', header=False, index=False)
-
-            sum_rows += len(rows)
-            logging.info(f'В {Parameter.table_name}.csv выгружено {sum_rows}  строк..')
-            if not rows:
-                print(f'ТАБЛИЦА ВЫГРУЖЕНА В {Parameter.table_name}.csv')
-                break
-
-
-SQL_REPS_CR_RES_RT = f"""select c_thrd_num, c_dog_index, c_dog_ref, c_debt_code,
+SQL = {
+    'REPS_CR_RES_RT': f"""select c_thrd_num, c_dog_index, c_dog_ref, c_debt_code,
                                 c_acc_num, c_acc_ref, c_res_prc, c_summ_res,
                                 c_base_acc_num, c_base_acc_ref, c_summ_debt#summ_rub,
                                 c_summ_debt#summ_val, c_line_ref, c_res_acc_saldo,
                                 c_res_prod, c_gr_risk, c_kind_res, c_summ_rasch_res,
                                 c_uch_debt_code, c_pos_num, c_pot_num, c_pot_ref, c_dog_class
                            from z#REPS_CR_RES_RT
-                          where c_pn = """
+                          where c_pn = """,
 
-SQL_REPS_RES_INFO_RT = """select t.c_prod_reserv, t.c_kind, t.c_summ_res, t.c_res_acc_ref, t.c_res_acc_num, t.c_debt_code,
+    'REPS_RES_INFO_RT': """select t.c_prod_reserv, t.c_kind, t.c_summ_res, t.c_res_acc_ref, t.c_res_acc_num, t.c_debt_code,
                                  t.c_res_prc, t.c_gr_risk, t.c_res_acc_saldo, t.c_pot_ref, t.c_port_num, t.o$acc_type,
                                  t.c_acc_corr_type, t.c_summ_corr, t.c_acc_positiv_ref, t.c_acc_negativ_ref, t.c_acc_negativ_num,
                                  t.c_acc_type_positiv, t.c_acc_type_negativ, t.c_dog_ref, t.c_corr_type, t.c_acc_type, t.c_acc_type, t.c_cr_rez_ln
                             from z#REPS_RES_INFO_RT t
-                           where t.c_pn = """
+                           where t.c_pn = """,
 
-SQL_REPS_DEBT_RT = """select t.c_dog_ref, t.c_acc_num, t.c_debt_code, t.c_line_ref, t.c_summ_debt#summ_rub, t.c_summ_debt#summ_val, t.c_date_close,
+    'REPS_DEBT_RT': """select t.c_dog_ref, t.c_acc_num, t.c_debt_code, t.c_line_ref, t.c_summ_debt#summ_rub, t.c_summ_debt#summ_val, t.c_date_close,
                              t.c_acc_ref, t.c_acc_saldo#summ_rub, t.c_acc_saldo#summ_val, t.c_main_acc_num, t.c_currency_code, t.c_currency_ref,
                              t.c_summ_gash#summ_rub, t.c_summ_gash#summ_val, t.c_summ_zlg#summ_val, t.c_summ_zlg#summ_rub, t.c_thrd_num, t.c_dog_index,
                              t.c_summ_plus#summ_rub, t.c_summ_plus#summ_val, t.c_summ_minus#summ_rub, t.c_summ_minus#summ_val, t.c_date_appearence,
@@ -87,9 +24,9 @@ SQL_REPS_DEBT_RT = """select t.c_dog_ref, t.c_acc_num, t.c_debt_code, t.c_line_r
                         from z#reps_debt_rt t, z#pr_cred c, z#kind_credits k
                         where c.id = t.c_dog_ref
                         and c.c_kind_credit = k.id
-                        and t.c_pn = """
+                        and t.c_pn = """,
 
-SQL_RT_NAVIGATOR = """SELECT C_1 Дата_расчета,
+    'RT_NAVIGATOR': """SELECT C_1 Дата_расчета,
                              C_2 Филиал,
                              C_3 Заемщик,
                              C_4 Класс_клиента,
@@ -160,4 +97,4 @@ SQL_RT_NAVIGATOR = """SELECT C_1 Дата_расчета,
                              C_73 Качество_обслуживания_долга,
                              C_74 Признак_чувствительности_к_изменению_процентной_ставки
                         FROM IBS.VW_CRIT_CRED
-                       WHERE C_76 = """
+                       WHERE C_76 = """}
