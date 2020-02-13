9b45d915bcd1caa0ea49e51a2d62a254812ab137
Valery Menshikov <48724158+ValeriyMenshikov@users.noreply.github.com>
2020-02-13 11:07:22 +0600

    Переписал используя кортеж

--- work_with_db.py
+++ work_with_db.py
@@ -2,21 +2,25 @@ import cx_Oracle
 import logging
 import pandas as pd
 
-
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 logging.disable(logging.INFO)
 
 
-def get_data_from_table(Parameter):
-    logging.debug(f'Устанавливаем соединение c {Parameter.db} установлено')
-    connection = cx_Oracle.connect(Parameter.db)
-    logging.debug(f'Соединение c {Parameter.db} установлено')
-    if Parameter.id_realization_2 is not None:
+def get_data_from_table(parameter):
+    table_name = parameter[0]
+    id_realization_1 = parameter[1]
+    id_realization_2 = parameter[2]
+    sql_query = parameter[3]
+    db = parameter[4]
+    logging.debug(f'Устанавливаем соединение c {db} установлено')
+    connection = cx_Oracle.connect(db)
+    logging.debug(f'Соединение c {db} установлено')
+    if id_realization_2 is not None:
         logging.debug('Формируем запрос для выгрузки результата сравнения реализации')
-        sql = Parameter.sql_query + Parameter.id_realization_1 + '\nminus\n' + Parameter.sql_query + Parameter.id_realization_2
+        sql = sql_query + id_realization_1 + '\nminus\n' + sql_query + id_realization_2
     else:
         logging.debug('Формируем запрос для выгрузки реализации')
-        sql = Parameter.sql_query + Parameter.id_realization_1
+        sql = sql_query + id_realization_1
 
     logging.debug(sql)
 
@@ -31,25 +35,19 @@ def get_data_from_table(Parameter):
 
         # Счетчик выгруженных строк
         sum_rows = 0
-        switch = True
-        while True:
+        rows = True
+        while rows:
             rows = cur.fetchmany(numRows)
             logging.debug('Запрос отправлен формируем датафрейм')
-
             df = pd.DataFrame(list(rows), columns=columns)
-            # Сначала записываем в файл с заголовком
-            if len(df) != 0:
-                switch = False
+            if rows:
+                # Сначала записываем в файл с заголовком
                 if sum_rows == 0:
-                    df.to_csv(f'{Parameter.table_name}.csv', mode='a', header=True, index=False)
+                    df.to_csv(f'{table_name}.csv', mode='a', header=True, index=False)
                 # Потом дописываем в файл без заголовка
                 else:
-                    df.to_csv(f'{Parameter.table_name}.csv', mode='a', header=False, index=False)
-            if switch:
-                print(f'В таблице {Parameter.table_name} расхождений нет')
-
+                    df.to_csv(f'{table_name}.csv', mode='a', header=False, index=False)
+            else:
+                print(f'В таблице {table_name} расхождений нет')
             sum_rows += len(rows)
-            logging.info(f'В {Parameter.table_name}.csv выгружено {sum_rows}  строк..')
-            if not rows:
-                # print(f'ТАБЛИЦА ВЫГРУЖЕНА В {Parameter.table_name}.csv')
-                break
+            logging.info(f'В {table_name}.csv выгружено {sum_rows}  строк..')
